659a8f51305c4420795a38e1b30cca7cc643a173
董家宏 <dongjiahong@hotmail.com>
2021-08-23 09:53:12 +0800

    delete target

--- contracts/market/target/debug/deps/market-529876f5712e3201.d
+++
@@ -1,5 +0,0 @@
-/fastdata/applications/near/nft-store/contracts/market/target/debug/deps/market-529876f5712e3201.rmeta: src/lib.rs
-
-/fastdata/applications/near/nft-store/contracts/market/target/debug/deps/market-529876f5712e3201.d: src/lib.rs
-
-src/lib.rs:
--- contracts/market/target/debug/deps/market-a02516d9f579a1b1.d
+++
@@ -1,5 +0,0 @@
-/fastdata/applications/near/nft-store/contracts/market/target/debug/deps/market-a02516d9f579a1b1.rmeta: src/lib.rs
-
-/fastdata/applications/near/nft-store/contracts/market/target/debug/deps/market-a02516d9f579a1b1.d: src/lib.rs
-
-src/lib.rs:
