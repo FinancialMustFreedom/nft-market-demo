c4a2214a5c887cf1bb0777c26138acc061343823
董家宏 <dongjiahong@hotmail.com>
2021-08-27 15:56:23 +0800

    update

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <h1>This is an store page</h1>
     <a-row type="flex" justify="space-around" align="middle">
       <a-col :span="5" v-for="(item, i) in metadatas" :key="i">
-        <img :src="item[0].metadata.media" width="320px" height="220px" />
+        <img :src="item.md[0].metadata.media" width="320px" height="220px" />
+        <div id="on-sale">售价：{{ item.show_price }} near</div>
+        <a-button id="buy" @click="buy_nft(item.md[0].token_id, item.price)"
+          >购买</a-button
+        >
         <a-divider></a-divider>
       </a-col>
     </a-row>
@@ -12,6 +15,10 @@
 
 <script>
 import utils from "../utils/near-utils";
+import {
+  formatNearAmount,
+  parseNearAmount,
+} from "near-api-js/lib/utils/format";
 
 const getSales = async () => {
   const sales = await window.wallet
@@ -34,7 +41,6 @@ const getMetadata = async (token_id) => {
     .viewFunction(utils.nearConfig.contractName, "nft_tokens_batch", {
       token_ids: [token_id],
     });
-  console.log("->>>>>>> token: ", token_id, " md: ", md);
   return md;
 };
 
@@ -43,12 +49,13 @@ export default {
   created: function () {
     getSales().then((sales) => {
       this.sales = sales;
-      console.log("---> sales: ", sales);
-      let i;
-      for (i in sales) {
+      for (let i in sales) {
         getMetadata(sales[i].token_id).then((md) => {
-          this.metadatas.push(md);
-          console.log("-----> md: ", md);
+          this.metadatas.push({
+            md,
+            price: sales[i].sale_conditions.near,
+            show_price: formatNearAmount(sales[i].sale_conditions.near, 2),
+          });
         });
       }
     });
@@ -59,6 +66,31 @@ export default {
       metadatas: [],
     };
   },
+  methods: {
+    buy_nft: async function (token_id, offerPrice) {
+      console.log("--> md: ", this.metadatas, " gas: ", utils.nearConfig.GAS);
+      this.$message.info(" token: " + token_id + offerPrice);
+      console.log("buy, token_id: ", token_id, " offerPrice", offerPrice);
+      if (offerPrice == 0) {
+        offerPrice = "0.0001";
+      }
+      await window.wallet
+        .account()
+        .functionCall(
+          utils.marketNearConfig.contractName,
+          "offer",
+          {
+            nft_contract_id: utils.nearConfig.contractName,
+            token_id: token_id,
+          },
+          utils.nearConfig.GAS,
+          parseNearAmount(offerPrice)
+        )
+        .then(() => {
+          this.$set();
+        });
+    },
+  },
 };
 </script>
 
@@ -66,5 +98,13 @@ export default {
 #h220 {
   height: 220px;
   background: greenyellow;
+}
+#on-sale {
+  margin-top: 10px;
+  margin-left: 40px;
+  float: left;
+}
+#buy {
+  margin-top: 10px;
 }
 </style>
